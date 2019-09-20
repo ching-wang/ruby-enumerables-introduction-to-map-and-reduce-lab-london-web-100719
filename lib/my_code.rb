43288f26b9f2026b269f175307072a7c6bfbb4d3
Qing Wang <chingwang.qw@gmail.com>
2019-09-20 22:36:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ def map_to_square(source_array)
   return source_array.map {|n| n * n}
 end 
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   source_array.reduce(starting_point) {|sum, n| sum + n }
 end 
 
